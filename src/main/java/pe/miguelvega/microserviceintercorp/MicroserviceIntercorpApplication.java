86b038f44ff8af0d09375625b6718f88621482d1
Miguel Vega <mvegaplm@gmail.com>
2019-08-02 22:11:44 -0500

    base project

---
+++ src/main/java/pe/miguelvega/microserviceintercorp/MicroserviceIntercorpApplication.java
@@ -0,0 +1,13 @@
+package pe.miguelvega.microserviceintercorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceIntercorpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceIntercorpApplication.class, args);
+    }
+
+}
